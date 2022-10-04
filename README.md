a80d3b74c407ef3e88c0b3965411df5b0fe26fd4
Samuel Müller <mueller.samu@gmail.com>
2022-10-04 21:32:18 +0200

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# cursor-placement
+
+A VSCode extension which helps with cursor placement:
+
+- if the cursor is in the currently visible range, the editor will scroll so
+  that the cursor is close to the top of the screen (5 lines under the top of
+  the editor window)
+
+- if the cursor is not in the currently visible range, the cursor will be brought
+  into the center of that range
---
+++ src/extension.ts
@@ -0,0 +1,36 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "cursor-placement.move",
+    () => {
+      const lineNumber = vscode.window.activeTextEditor?.selection.start.line;
+
+      const [range] = vscode.window.activeTextEditor?.visibleRanges ?? [];
+
+      if (!range || lineNumber === undefined) {
+        return;
+      }
+
+      const startLine = range.start.line;
+      const endLine = range.end.line;
+
+      const isInRange = startLine <= lineNumber && lineNumber <= endLine;
+
+      if (isInRange) {
+        vscode.commands.executeCommand("revealLine", {
+          lineNumber: lineNumber - 5,
+          at: "top",
+        });
+      } else {
+        vscode.commands.executeCommand("cursorMove", {
+          to: "viewPortCenter",
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
